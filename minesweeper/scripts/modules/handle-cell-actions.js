ffb3eb716eb0e5ed8f24a9673196770b3b34232e
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-05-11 13:21:26 +0300

    feat: show all cells when bobm

--- minesweeper/scripts/modules/handle-cell-actions.js
+++ minesweeper/scripts/modules/handle-cell-actions.js
@@ -1,21 +1,33 @@
 import { returnFinishArray } from './add-cells.js';
 
-function showCell(id, textContent, value) {
+function showNumberCell(id, textContent, value) {
   const element = document.getElementById(id);
   element.textContent = textContent;
   element.classList.add(value);
 }
 
+function showAllCells(array) {
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell, index) => {
+    const element = cell;
+    element.classList.add(array[index]);
+    if (array[index] === 'bomb') {
+      element.textContent = '🐹';
+    } else if (array[index].slice(0, -1) === 'number') {
+      element.textContent = array[index].slice(-1);
+    }
+  });
+}
 function handleCellAction(id) {
   const array = returnFinishArray();
   const cellValue = array[id];
   if (cellValue === 'bomb') {
-    const textContent = '🐼';
-    showCell(id, textContent, cellValue);
+    console.log('Game over');
+    showAllCells(array);
   }
   if (cellValue.slice(0, -1) === 'number') {
     const number = cellValue.slice(-1);
-    showCell(id, number, cellValue);
+    showNumberCell(id, number, cellValue);
   }
   if (cellValue === 'simple') {
     console.log(cellValue);
